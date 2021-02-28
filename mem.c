abecf1bf1e598da5b305abcd276bd41e4028947a
gilmgoncalves <gil@fe.up.pt>
2021-02-28 00:05:11 +0000

    Update mem.c

--- mem.c
+++ mem.c
@@ -7,10 +7,11 @@
 int
 main(int argc, char *argv[])
 {
-    if (argc != 2) { 
-	fprintf(stderr, "usage: mem <value>\n"); 
-	exit(1); 
+    if (argc != 2) {
+        fprintf(stderr, "usage: mem <value>\n"); 
+        exit(1); 
     } 
+    
     int *p;                   // memory for pointer is on "stack"
     p = malloc(sizeof(int));  // malloc'd memory is on "heap"
     assert(p != NULL);
@@ -19,9 +20,9 @@ main(int argc, char *argv[])
     printf("(pid:%d) addr stored in p: %llx\n", (int) getpid(), (unsigned long long) p);
     *p = atoi(argv[1]); // assign value to addr stored in p
     while (1) {
-	Spin(1);
-	*p = *p + 1;
-	printf("(pid:%d) value of p: %d\n", getpid(), *p);
+        Spin(1);
+        *p = *p + 1;
+        printf("(pid:%d) value of p: %d\n", getpid(), *p);
     }
 
     return 0;
